1213d14b8d4b299fd1e538f2318c64fab6aee5de
Adilet Kasimov <kasimoff17@gmail.com>
2024-02-13 20:10:32 +0300

    commit

---
+++ main.js
@@ -0,0 +1,107 @@
+const apiResponse = {
+  services: [
+    {
+      id: 1,
+      head: null,
+      name: "Проф.осмотр",
+      node: 0,
+      price: 100.0,
+      sorthead: 20,
+    },
+    {
+      id: 2,
+      head: null,
+      name: "Хирургия",
+      node: 1,
+      price: 0.0,
+      sorthead: 10,
+    },
+    {
+      id: 3,
+      head: 2,
+      name: "Удаление зубов",
+      node: 1,
+      price: 0.0,
+      sorthead: 10,
+    },
+    {
+      id: 4,
+      head: 3,
+      name: "Удаление зуба",
+      node: 0,
+      price: 800.0,
+      sorthead: 10,
+    },
+    {
+      id: 5,
+      head: 3,
+      name: "Удаление 8ого зуба",
+      node: 0,
+      price: 1000.0,
+      sorthead: 30,
+    },
+    {
+      id: 6,
+      head: 3,
+      name: "Удаление осколка зуба",
+      node: 0,
+      price: 2000.0,
+      sorthead: 20,
+    },
+    {
+      id: 7,
+      head: 2,
+      name: "Хирургические вмешательство",
+      node: 0,
+      price: 200.0,
+      sorthead: 10,
+    },
+    {
+      id: 8,
+      head: 2,
+      name: "Имплантация зубов",
+      node: 1,
+      price: 0.0,
+      sorthead: 20,
+    },
+    {
+      id: 9,
+      head: 8,
+      name: "Коронка",
+      node: 0,
+      price: 3000.0,
+      sorthead: 10,
+    },
+    {
+      id: 10,
+      head: 8,
+      name: "Слепок челюсти",
+      node: 0,
+      price: 500.0,
+      sorthead: 20,
+    },
+  ],
+};
+
+function buildTree(data, parent, level) {
+  const ul = document.createElement("ul");
+
+  data
+    .filter((item) => item.head === parent)
+    .sort((a, b) => a.sorthead - b.sorthead)
+    .forEach((item) => {
+      const li = document.createElement("li");
+      li.textContent = `${item.name} (${item.price})`;
+
+      if (item.node === 1) {
+        const nestedUl = buildTree(data, item.id, level + 1);
+        li.appendChild(nestedUl);
+      }
+      ul.appendChild(li);
+    });
+  return ul;
+}
+
+const treeContainer = document.getElementById("tree");
+const tree = buildTree(apiResponse.services, null, 0);
+treeContainer.appendChild(tree);
